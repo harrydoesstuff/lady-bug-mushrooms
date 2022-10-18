da5a812d1a737e9ee9fef58c9e6298c31b7a629a
Harry Smith <harryawsmith@gmail.com>
2022-10-18 14:38:24 +1100

    added temp home button in nav until I decide the final nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
+        
         <div className="navbar bg-gradient-to-r from-indigo-300 to-purple-400 flex justify-center drop-shadow-2xl rounded-b-full opacity-90 absolute">
+            {/* <a className="btn normal-case text-xl">LadyBug Mushrooms</a> */}
             <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
+                <button className="btn hover:bg-primary-focus"><Link to="/">Home</Link></button>
                 <button className="btn hover:bg-primary-focus"><Link to="/store">Store</Link></button>
                 <button className="btn hover:bg-primary-focus"><Link to="/recipes">Recipes</Link></button>
                 <button className="btn hover:bg-primary-focus"><Link to="/aboutus">About Us</Link></button>
